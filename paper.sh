c6d0c71b5155ec44610a2bbb56abf44514cb18b2
Cory Borek <agentdid127cb@aol.com>
2025-06-11 00:14:26 +0000

    fix paper and velocity install scripts

--- paper.sh
+++ paper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PAPER_VERSION=1.21.4
+PAPER_VERSION="1.21.4"
 if [[ -n $1 ]]; then
 PAPER_VERSION=$1
 fi
 
 wget https://raw.githubusercontent.com/CoryBorek/mc-server-scripts/refs/heads/main/papermc.sh
 chmod +x ./papermc.sh
-./papermc.sh paper $PAPER_VERSION
+./papermc.sh "paper" $PAPER_VERSION
 rm papermc.sh
 echo "eula=true" > eula.txt
--- velocity.sh
+++ velocity.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-VELOCITY_VERSION=3.4.0-SNAPSHOT
+VELOCITY_VERSION="3.4.0-SNAPSHOT"
 if [[ -n $1 ]]; then
 VELOCITY_VERSION=$1
 fi
 
 wget https://raw.githubusercontent.com/CoryBorek/mc-server-scripts/refs/heads/main/papermc.sh
 chmod +x ./papermc.sh
-./papermc.sh velocity $VELOCITY_VERSION
+./papermc.sh "velocity" $VELOCITY_VERSION
 rm papermc.sh
-echo "eula=true" > eula.txt
